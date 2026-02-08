3c7eaf3ea17b05baf7adf8d9e54933ffbcf95bb2
akashdange60 <148521750+akashdange60@users.noreply.github.com>
2026-02-08 12:28:27 +0530

    initial commit

---
+++ src/main/java/com/async_event/SpringbootAsyncEventBatchApplication.java
@@ -0,0 +1,13 @@
+package com.async_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAsyncEventBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAsyncEventBatchApplication.class, args);
+	}
+
+}
